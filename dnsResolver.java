3542116e9c06ff406186365a9584b1974652c4af
Nexhmedin <nexhmedin.veizi1@gmail.com>
2023-07-26 12:56:31 +1000

    can now retrieve NS records

--- dnsResolver.java
+++ dnsResolver.java
@@ -15,7 +15,6 @@ public class dnsResolver {
         int port = Integer.parseInt(args[0]);
         DatagramSocket socket = new DatagramSocket(port);
         InetAddress ipAddress = InetAddress.getByName("125.253.99.33");
-        
         // need to regex root file to get ipv4 addresses
 
         while (true) {
@@ -112,57 +111,54 @@ public class dnsResolver {
                 String ipFinal = ip.toString();
                 System.out.println(ipFinal);
             }
-
+            System.out.println("\n");
+            // holds NS records -> skip if we have A records in the additional section
+            // otherwise need to process one of the NS records
 
             for (int i = 0; i < NSCOUNT; i++) {
                 skipQname(dataInputStream);
-                ArrayList<Integer> RDATA = new ArrayList<>();
                 short TYPE = dataInputStream.readShort();
                 short CLASS = dataInputStream.readShort();
                 int TTL = dataInputStream.readInt();
                 int RDLENGTH = dataInputStream.readShort();
-                for(int s = 0; s < RDLENGTH; s++) {
-                    int nx = dataInputStream.readByte() & 255;// and with 255 to
-                    RDATA.add(nx);
-                }
 
                 System.out.println("Type: " + TYPE);
                 System.out.println("Class: " + CLASS);
                 System.out.println("Time to live: " + TTL);
                 System.out.println("Rd Length: " + RDLENGTH);
-                StringBuilder ip = new StringBuilder();
-                for (Integer ipPart:RDATA) {
-                    ip.append(ipPart).append(".");
-                }
-                ip.deleteCharAt(ip.length() - 1);
-                String ipFinal = ip.toString();
-                System.out.println(ipFinal);
+                byte[] rdata = new byte[RDLENGTH];
+                dataInputStream.readFully(rdata);
+                String NSRecord = getNSRecord(rdata, 0, response);
+                System.out.println(NSRecord);
+                System.out.println("\n");
             }
 
-            for (int i = 0; i < ARCOUNT; i++) {
-                skipQname(dataInputStream);
-                ArrayList<Integer> RDATA = new ArrayList<>();
-                short TYPE = dataInputStream.readShort();
-                short CLASS = dataInputStream.readShort();
-                int TTL = dataInputStream.readInt();
-                int RDLENGTH = dataInputStream.readShort();
-                for(int s = 0; s < RDLENGTH; s++) {
-                    int nx = dataInputStream.readByte() & 255;// and with 255 to
-                    RDATA.add(nx);
-                }
+            // usually holds AAAA and A records
+            // skip all AAAA records and query first A record we come across. 
+            // for (int i = 0; i < ARCOUNT; i++) {
+            //     skipQname(dataInputStream);
+            //     ArrayList<Integer> RDATA = new ArrayList<>();
+            //     short TYPE = dataInputStream.readShort();
+            //     short CLASS = dataInputStream.readShort();
+            //     int TTL = dataInputStream.readInt();
+            //     int RDLENGTH = dataInputStream.readShort();
+            //     for(int s = 0; s < RDLENGTH; s++) {
+            //         int nx = dataInputStream.readByte() & 255;// and with 255 to
+            //         RDATA.add(nx);
+            //     }
 
-                System.out.println("Type: " + TYPE);
-                System.out.println("Class: " + CLASS);
-                System.out.println("Time to live: " + TTL);
-                System.out.println("Rd Length: " + RDLENGTH);
-                StringBuilder ip = new StringBuilder();
-                for (Integer ipPart:RDATA) {
-                    ip.append(ipPart).append(".");
-                }
-                ip.deleteCharAt(ip.length() - 1);
-                String ipFinal = ip.toString();
-                System.out.println(ipFinal);
-            }
+            //     System.out.println("Type: " + TYPE);
+            //     System.out.println("Class: " + CLASS);
+            //     System.out.println("Time to live: " + TTL);
+            //     System.out.println("Rd Length: " + RDLENGTH);
+            //     StringBuilder ip = new StringBuilder();
+            //     for (Integer ipPart:RDATA) {
+            //         ip.append(ipPart).append(".");
+            //     }
+            //     ip.deleteCharAt(ip.length() - 1);
+            //     String ipFinal = ip.toString();
+            //     System.out.println(ipFinal);
+            // }
             
         }
     }
@@ -182,40 +178,55 @@ public class dnsResolver {
         // if we have > 0 in additional section get that IP and send the next query
         // if we have = 0 then send A packet for record in authoritative section
         // if answer -> send to client
-	
-    private static void printData(DatagramPacket request) throws Exception
-   {
-      // Obtain references to the packet's array of bytes.
-      byte[] buf = request.getData();
 
-      // Wrap the bytes in a byte array input stream,
-      // so that you can read the data as a stream of bytes.
-      ByteArrayInputStream bais = new ByteArrayInputStream(buf);
 
-      // Wrap the byte array output stream in an input stream reader,
-      // so you can read the data as a stream of characters.
-      InputStreamReader isr = new InputStreamReader(bais);
+    private static boolean checkServerError(DataInputStream dataInputStream) throws IOException {
+        return false;
+    }
 
-      // Wrap the input stream reader in a bufferred reader,
-      // so you can read the character data a line at a time.
-      // (A line is a sequence of chars terminated by any combination of \r and \n.) 
-      BufferedReader br = new BufferedReader(isr);
+    private static boolean checkOtherErrors(DataInputStream dataInputStream) throws IOException {
+        return false;  
+    }
 
-      // The message data is contained in a single line, so read this line.
-      String line = br.readLine();
+    private static ArrayList<String> getIpAddresses(DataInputStream dataInputStream) {
+        return new ArrayList<>();
+    }
 
-      // Print host address and data received from it.
-      System.out.println(
-         "Received from " + 
-         request.getAddress().getHostAddress() + 
-         ": " +
-         new String(line) );
-   }
+    private static String getNSRecord(byte[] rdata, int index, byte[] response) {
+        StringBuilder nsRecord = new StringBuilder();
+
+        int i = index;
+        int length = rdata.length;
+        while (i < length) {
+            int labelLength = rdata[i++];
+            if (labelLength == 0) {
+                break;
+            }
+
+            if (nsRecord.length() > 0) {
+                nsRecord.append(".");
+            }
+
+            if ((labelLength & 0b11000000) == 0b11000000) {
+                // Compressed label, jump to the offset specified in the next byte
+                int offset = ((labelLength & 0b00111111) << 8) + (rdata[i++] & 0b11111111);
+                String label = getNSRecord(response, offset, response); // Recursive call to continue parsing after compression
+                nsRecord.append(label);
+                break;
+            } else {
+                // Non-compressed label
+                for (int j = 0; j < labelLength; j++) {
+                    nsRecord.append((char) rdata[i++]);
+                }
+            }
+        }
+
+        return nsRecord.toString();
+    }
 
     private static void skipQname(DataInputStream dataInputStream) throws IOException {
         int labelLength;
         while ((labelLength = dataInputStream.readUnsignedByte()) > 0) {
-        // If the first two bits of label length are 11 (11000000 in binary), it's a compression pointer.
             if ((labelLength & 0xC0) == 0xC0) {
                 dataInputStream.skipBytes(1);
                 break;
